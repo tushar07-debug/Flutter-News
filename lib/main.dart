cf3b0ce45380a538a46c03eb58c92f39d23f9123
Tushar Kumar Singh <tusharsingh102003@gmail.com>
2024-03-05 22:38:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutternews/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const Home(),
+    );
+  }
+}
---
+++ lib/models/article_model.dart
@@ -0,0 +1,11 @@
+class ArticleModel{
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String content;
+
+  ArticleModel({required this.author, required this.title, required this.url, required this.description, required this.content, required this.urlToImage});
+}
